d3e736525a9479e7221c17401bb65946bba60285
Alvaro Inckot <alvaro.inckot@nelnet.net>
2020-11-07 03:57:19 -0300

    some mods

--- main.py
+++ main.py
@@ -12,16 +12,19 @@ logging.info("Poker Analiser v{}".format(os.getenv("VERSION")))
 logging.debug("Hand history at {}".format(os.getenv("HAND_HISTORY_PATH")))
 
 
-histories = read_all_tournaments()
+tournaments = read_all_tournaments()
 total = []
 
-for history in histories:  # enumerable
-    for hand in interpret(history):  # enumerable
-        # https://github.com/ihendley/treys/blob/master/treys/card.py
-        print(hand.id)  # should consolidate to a csv
-        total = total + hand.actions
+for tournament in tournaments:  # enumerable
+    try:
+        for hand in interpret(tournament):  # enumerable
+            print(hand.id)  # should consolidate to a csv
+            total = total + hand.actions
+    except:
+        print("Something bad happened....")
+        continue
 
 
 df = pd.DataFrame(total).fillna(0)
 print(df)
-df.to_csv("./compilations/summary_v1.csv", index=None, header=True)
+df.to_csv("./compilations/summary_v3.csv", index=None, header=True)
