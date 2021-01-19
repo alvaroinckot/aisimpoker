553325a4a3ec6fef63870927f21c32dcd8edb274
Alvaro Cantador <4078365+alvarocantador@users.noreply.github.com>
2021-01-18 21:17:42 -0300

    removed obsolete code

--- dsl/semantic.py
+++ dsl/semantic.py
@@ -22,13 +22,9 @@ class PokerSemantic(Transformer):
     def received_card(self, token):
         self._tournament.current_match.set_hero(token[0])
         self._tournament.current_match.set_hand_card(token[1].children)
-        # self._tournament.current_match.set_hero_position(token[0])
 
     def chips(self, token):
         return ast.literal_eval(token[0])
-
-    # def total_pot(self, token):
-        # print(token[0])
 
     def seat(self, token):
         seat = {'name': token[1], 'chips': token[2], 'seat': ast.literal_eval(
@@ -78,18 +74,15 @@ class PokerSemantic(Transformer):
                     action.children[0])
 
     def raised(self, token):
-        # self._tournament.current_match.pot += ast.literal_eval(token[1])
         return {'action': 'raise', 'player': token[0], 'action_chips': ast.literal_eval(token[1])}
 
     def bet(self, token):
-        # self._tournament.current_match.pot += ast.literal_eval(token[1])
         return {'action': 'raise', 'player': token[0], 'action_chips': ast.literal_eval(token[1])}
 
     def fold(self, token):
         return {'action': 'fold', 'player': token[0], 'action_chips': 0}
 
     def call(self, token):
-        # self._tournament.current_match.pot += ast.literal_eval(token[1])
         return {'action': 'call', 'player': token[0], 'action_chips': ast.literal_eval(token[1])}
 
     def check(self, token):
@@ -130,53 +123,3 @@ class PokerSemantic(Transformer):
 
     def show_down_player(self, token):
         return None
-
-    # def total_pot(self, token):
-    # # print(token[0])
-    # # return ast.literal_eval(token[0])
-    # return token[0]
-
-    # def total_pot_many(self, token):
-    # return ast.literal_eval(token[0])
-
-    # def total_pot_single(self, token):
-    # return ast.literal_eval(token[0])
-
-    # def _street_actions(self, token, street):
-    # act = 0
-    # act_array = []
-    # action_array = []
-    # for i, v in enumerate(token):
-    # action = v.children[0]
-    # if isinstance(action, type(None)) or isinstance(action, Tree) or action['action'] == 'timeout':
-    # continue
-    # act_array.append(action['player'])
-    # act = act_array.count(action['player'])
-    # action_array.append(
-    # {'player': action['player'], 'type': action['action'], 'id': i, 'act': act, 'position': ''})
-
-    # # Position
-    # positions = ['BB', 'SB', 'BTN', 'CO',
-    #  'HJ', 'MP2', 'MP1', 'UTG+1', 'UTG']
-    # i = 0
-    # for a in reversed(action_array):
-    # a['position'] = positions[i]
-
-    # if i == 0:
-    # first_a = a
-
-    # if a['act'] != act:
-    # if i == 2:
-    # first_a['position'] = positions[2]
-    # a['position'] = positions[0]
-    # act = a['act']
-    # i = 1
-    # else:
-    # i = i + 1
-
-    # return action_array
-
-    # Add Fact (just to add in order. Could be part of the prior FOR
-    # for a in action_array:
-    # self.engine.declare(Action(street=street, me=False, is_raised=False,
-    #    player=a['player'], type=a['type'], id=a['id'], act=a['act'], position=a['position']))
